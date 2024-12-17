76b739c7277c38c22ea60f79be4f2537528f003d
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2024-12-17 20:18:07 +0100

    basics working

--- src/main.py
+++ src/main.py
@@ -1,25 +1,32 @@
 import asyncio
 import sys
-from typing import ClassVar, Final, List, Mapping, Optional, Sequence
+from typing import Any, ClassVar, Final, List, Mapping, Optional, Sequence
 
 from typing_extensions import Self
-from viam.media.video import ViamImage
+from viam.media.video import ViamImage, CameraMimeType
 from viam.module.module import Module
 from viam.proto.app.robot import ComponentConfig
 from viam.proto.common import PointCloudObject, ResourceName
-from viam.proto.service.vision import (Classification, Detection,
-                                       GetPropertiesResponse)
+from viam.proto.service.vision import Classification, Detection, GetPropertiesResponse
 from viam.resource.base import ResourceBase
 from viam.resource.easy_resource import EasyResource
 from viam.resource.types import Model, ModelFamily
 from viam.services.vision import *
 from viam.utils import ValueTypes
+from viam.components.camera import Camera
+from viam.media.utils.pil import viam_to_pil_image, pil_to_viam_image
+
+import cv2
+import numpy as np
+from PIL import Image
 
 
 class Sealant(Vision, EasyResource):
     MODEL: ClassVar[Model] = Model(
         ModelFamily("hpe-automotive", "sealant-check"), "sealant"
     )
+
+    camera: Camera
 
     @classmethod
     def new(
@@ -59,6 +66,7 @@ class Sealant(Vision, EasyResource):
             config (ComponentConfig): The new configuration
             dependencies (Mapping[ResourceName, ResourceBase]): Any dependencies (both implicit and explicit)
         """
+        self.camera = dependencies[Camera.get_resource_name("camera")]
         return super().reconfigure(config, dependencies)
 
     async def capture_all_from_camera(
@@ -69,17 +77,22 @@ class Sealant(Vision, EasyResource):
         return_detections: bool = False,
         return_object_point_clouds: bool = False,
         *,
-        extra: Optional[Mapping[str, ValueTypes]] = None,
-        timeout: Optional[float] = None
+        extra: Optional[Mapping[str, Any]] = None,
+        timeout: Optional[float] = None,
     ) -> CaptureAllResult:
-        raise NotImplementedError()
+        if self.camera.name != camera_name:
+            raise ValueError("Camera not added as dependency")
+        cam_image = await self.camera.get_image()
+        result = CaptureAllResult()
+        result.image = self.process_image(cam_image)
+        return result
 
     async def get_detections_from_camera(
         self,
         camera_name: str,
         *,
         extra: Optional[Mapping[str, ValueTypes]] = None,
-        timeout: Optional[float] = None
+        timeout: Optional[float] = None,
     ) -> List[Detection]:
         raise NotImplementedError()
 
@@ -88,7 +101,7 @@ class Sealant(Vision, EasyResource):
         image: ViamImage,
         *,
         extra: Optional[Mapping[str, ValueTypes]] = None,
-        timeout: Optional[float] = None
+        timeout: Optional[float] = None,
     ) -> List[Detection]:
         raise NotImplementedError()
 
@@ -98,7 +111,7 @@ class Sealant(Vision, EasyResource):
         count: int,
         *,
         extra: Optional[Mapping[str, ValueTypes]] = None,
-        timeout: Optional[float] = None
+        timeout: Optional[float] = None,
     ) -> List[Classification]:
         raise NotImplementedError()
 
@@ -108,7 +121,7 @@ class Sealant(Vision, EasyResource):
         count: int,
         *,
         extra: Optional[Mapping[str, ValueTypes]] = None,
-        timeout: Optional[float] = None
+        timeout: Optional[float] = None,
     ) -> List[Classification]:
         raise NotImplementedError()
 
@@ -117,7 +130,7 @@ class Sealant(Vision, EasyResource):
         camera_name: str,
         *,
         extra: Optional[Mapping[str, ValueTypes]] = None,
-        timeout: Optional[float] = None
+        timeout: Optional[float] = None,
     ) -> List[PointCloudObject]:
         raise NotImplementedError()
 
@@ -125,11 +138,50 @@ class Sealant(Vision, EasyResource):
         self,
         *,
         extra: Optional[Mapping[str, ValueTypes]] = None,
-        timeout: Optional[float] = None
+        timeout: Optional[float] = None,
     ) -> Vision.Properties:
         raise NotImplementedError()
+
+    async def do_command(
+        self, command, *, timeout=None, **kwargs
+    ) -> Mapping[str, ValueTypes]:
+        # img = self.camera.get_image()
+        # self.prepare_image(img)
+
+        result = {"felix": "test"}
+        return result
+
+    def process_image(self, cam_image: ViamImage) -> ViamImage:
+        # Convert the ViamImage to a PIL image
+        pil_image = viam_to_pil_image(cam_image)
+        # Convert the PIL image to a NumPy array
+        np_image = np.array(pil_image)
+        # Convert RGB to BGR (OpenCV uses BGR by default)
+        if np_image.ndim == 3 and np_image.shape[2] == 3:
+            np_image = cv2.cvtColor(np_image, cv2.COLOR_RGB2BGR)
+        # Convert the NumPy array to a grayscale image
+        gray_image = cv2.cvtColor(np_image, cv2.COLOR_BGR2GRAY)
+        # Threshold the image to create a binary image (black and white)
+        _, bw_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_OTSU)
+        cv2.imwrite("bw_image.jpg", bw_image)
+        wb_image = cv2.bitwise_not(bw_image)
+        cv2.imwrite("wb_image.jpg", wb_image)
+        # Convert the NumPy array back to a PIL image
+        pil_image = matlike_to_pil(wb_image)
+        # Convert the PIL image to a ViamImage
+        result_image = pil_to_viam_image(pil_image, CameraMimeType.JPEG)
+        return result_image
+
+
+def matlike_to_pil(np_image: np.ndarray) -> Image.Image:
+    np_image = cv2.cvtColor(np_image, cv2.COLOR_GRAY2RGB)
+    # Convert BGR to RGB (OpenCV uses BGR by default)
+    if np_image.ndim == 3 and np_image.shape[2] == 3:
+        np_image = cv2.cvtColor(np_image, cv2.COLOR_BGR2RGB)
+    # Convert the NumPy array to a PIL image
+    pil_image = Image.fromarray(np_image)
+    return pil_image
 
 
 if __name__ == "__main__":
     asyncio.run(Module.run_from_registry())
-
